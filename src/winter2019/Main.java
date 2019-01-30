fd6b6f3995dafc4695036017c0cde21e98eec1bd
Hansaem Suh <hansammy.suh@gmail.com>
2019-01-30 16:03:27 +0900

    Initial commit

---
+++ src/winter2019/Main.java
@@ -0,0 +1,27 @@
+package winter2019;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("a");
+		
+		Main test = new Main();
+		
+		
+		test.a();
+		
+	}
+	
+
+
+	public void a() {
+		System.out.println("b");
+		System.out.println("c");
+		System.out.println("d");
+		System.out.println("e");
+	}
+
+}
+
